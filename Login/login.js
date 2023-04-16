b1149d4d9eb4b8eddef9940d166186d289bc3d7f
Shubham <privatestartup2004@gmail.com>
2023-04-17 00:23:58 +0530

    Login Added

--- Login/login.js
+++ Login/login.js
@@ -1,23 +1,11 @@
-var emailArray = ['shubhamvarma2004@gmail.com', 'test'];
-var passwordArray = ['Shubham@1985', 'test'];
+var emailArray = ['user'];
+var passwordArray = ['user'];
+
+// var emailArrayAdmin = ['shubhamvarma2004@gmail.com'];
+// var passwordArrayAdmin = ['Shubham@1985'];
 
 var loginBox = document.getElementById("login");
-var regBox = document.getElementById("register");
-var forgetBox = document.getElementById("forgot");
 
-var loginTab = document.getElementById("lt");
-var regTab = document.getElementById("rt");
-
-function regTabFun() {
-    event.preventDefault();
-
-    regBox.style.visibility = "visible";
-    loginBox.style.visibility = "hidden";
-    forgetBox.style.visibility = "hidden";
-
-    regTab.style.backgroundColor = "rgb(12, 132, 189)";
-    loginTab.style.backgroundColor = "rgba(11, 177, 224, 0.82)";
-}
 function loginTabFun() {
     event.preventDefault();
 
@@ -28,50 +16,7 @@ function loginTabFun() {
     loginTab.style.backgroundColor = "rgb(12, 132, 189)";
     regTab.style.backgroundColor = "rgba(11, 177, 224, 0.82)";
 }
-function forTabFun() {
-    event.preventDefault();
 
-    regBox.style.visibility = "hidden";
-    loginBox.style.visibility = "hidden";
-    forgetBox.style.visibility = "visible";
-
-    regTab.style.backgroundColor = "rgba(11, 177, 224, 0.82)";
-    loginTab.style.backgroundColor = "rgba(11, 177, 224, 0.82)";
-}
-
-function register() {
-    event.preventDefault();
-
-    var email = document.getElementById("re").value;
-    var password = document.getElementById("rp").value;
-    var passwordRetype = document.getElementById("rrp").value;
-
-    if (email == "") {
-        alert("Email required.");
-        return;
-    } else if (password == "") {
-        alert("Password required.");
-        return;
-    } else if (passwordRetype == "") {
-        alert("Password required.");
-        return;
-    } else if (password != passwordRetype) {
-        alert("Password don't match retype your Password.");
-        return;
-    } else if (emailArray.indexOf(email) == -1) {
-        emailArray.push(email);
-        passwordArray.push(password);
-
-        alert(email + "  Thanks for registration. \nTry to login Now");
-
-        document.getElementById("re").value = "";
-        document.getElementById("rp").value = "";
-        document.getElementById("rrp").value = "";
-    } else {
-        alert(email + " is already register.");
-        return;
-    }
-}
 function login() {
     event.preventDefault();
 
@@ -95,28 +40,54 @@ function login() {
         alert("Password does not match.");
         return;
     } else {
-        alert(email + " yor are login Now \n welcome to our website.");
-        location.href = '../Clients/user.html';
-
+        alert(email + " You Logged In User Panel Successfully");
+        // window.history.replaceState('../Clients/user.html');
+        location.assign('../Clients/user.html')
+        location.replace('../Clients/user.html')
+        // window.history.replaceState(nextState, nextTitle, nextURL);
+        // window.history.pushState(nextState, nextTitle, nextURL);
         document.getElementById("se").value = "";
         document.getElementById("sp").value = "";
         return;
     }
 }
-function forgot() {
-    event.preventDefault();
 
-    var email = document.getElementById("fe").value;
+// Current URL: https://my-website.com/page_a
+const nextURL = '../Clients/user.html';
+const nextTitle = 'User Panel';
+const nextState = { additionalInformation: 'Updated the URL with JS' };
 
-    if (emailArray.indexOf(email) == -1) {
-        if (email == "") {
-            alert("Email required.");
-            return;
-        }
-        alert("Email does not exist.");
-        return;
-    }
+// This will create a new entry in the browser's history, without reloading
 
-    alert("email is send to your email check it in 24hr. \n Thanks");
-    document.getElementById("fe").value = "";
-}
+// This will replace the current entry in the browser's history, without reloading
+
+// function login() {
+//     event.preventDefault();
+
+//     var email = document.getElementById("se").value;
+//     var password = document.getElementById("sp").value;
+
+//     var i = emailArrayAdmin.indexOf(email);
+
+//     if (emailArrayAdmin.indexOf(email) == -1) {
+//         if (email == "shubhamvara2004@gmail.com") {
+//             alert("Email required.");
+//             return;
+//         }
+//         alert("Email does not exist.");
+//         return;
+//     } else if (passwordArrayAdmin[i] != password) {
+//         if (password == "Shubham@1985") {
+//             alert("Password required.");
+//             return;
+//         }
+//         alert("Password does not match.");
+//         return;
+//     } else {
+//         alert(email + " You Logged In Admin Panel Successfully");
+//         location.href = '../Clients/admin.html';
+//         document.getElementById("se").value = "";
+//         document.getElementById("sp").value = "";
+//         return;
+//     }
+// }
