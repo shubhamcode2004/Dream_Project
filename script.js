64437ae1a19da2b7a9223a66ca2ff84498782dc2
Shubham <privatestartup2004@gmail.com>
2023-04-22 00:15:14 +0530

    All Sections Added and Optimized fro Responsiveness

--- script.js
+++ script.js
@@ -66,17 +66,46 @@ window.onload = function () {
     css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
     document.body.appendChild(css);
 };
+window.addEventListener("DOMContentLoaded", function () {
+    // get the form elements defined in your form HTML above
 
-// Dark & Light toggle
+    var form = document.getElementById("my-form");
+    // var button = document.getElementById("my-form-button");
+    var status = document.getElementById("status");
 
-document.querySelector(".day-night input").addEventListener("change", () => {
-    document.querySelector("body").classList.add("toggle");
-    setTimeout(() => {
-        document.querySelector("body").classList.toggle("light");
+    // Success and Error functions for after the form is submitted
 
-        setTimeout(
-            () => document.querySelector("body").classList.remove("toggle"),
-            10
-        );
-    }, 5);
+    function success() {
+        form.reset();
+        alert("Submitted Sucessfully !");
+    }
+
+    function error() {
+        alert("OOPS ! There was some Problem");
+    }
+
+    // handle the form submission event
+
+    form.addEventListener("submit", function (ev) {
+        ev.preventDefault();
+        var data = new FormData(form);
+        ajax(form.method, form.action, data, success, error);
+    });
 });
+
+// helper function for sending an AJAX request
+
+function ajax(method, url, data, success, error) {
+    var xhr = new XMLHttpRequest();
+    xhr.open(method, url);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onreadystatechange = function () {
+        if (xhr.readyState !== XMLHttpRequest.DONE) return;
+        if (xhr.status === 200) {
+            success(xhr.response, xhr.responseType);
+        } else {
+            error(xhr.status, xhr.response, xhr.responseType);
+        }
+    };
+    xhr.send(data);
+}
