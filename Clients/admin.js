a0f93634ada900ce683b2884cda8d56c9480e249
Shubham <privatestartup2004@gmail.com>
2023-08-19 11:55:06 +0530

    User Panel Development Started

--- Clients/admin.js
+++ Clients/admin.js
@@ -1,3 +1,8 @@
-// window.onload = function () {
-//     location.href = "../index.html";
-// }
+function logout() {
+    let text = "ARE YOU SURE WANT TO LOG OUT ?\nEither OK or Cancel.";
+    if (confirm(text) == true) {
+        window.location.href = "../index.html";
+    } else {
+        console.log("You Cancelled");
+    }
+}
